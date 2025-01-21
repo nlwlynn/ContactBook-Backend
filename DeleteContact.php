3b5807f653ad6bbe0f4b0221f126630fb9ed83bf
jobritto7 <joannabritto1@gmail.com>
2025-01-21 16:04:34 -0500

    delete based on ID

---
+++ DeleteContact.php
@@ -0,0 +1,38 @@
+<?php
+
+	$inData = getRequestInfo();
+    $UserID = $inData["UserID"];
+	
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("delete * from Contacts where UserID= ?");
+		$stmt->bind_param("i", $UserID);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
