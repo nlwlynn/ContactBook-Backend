29f7cbbbd881ebf5cd86f620dc1e1d49a897ec64
Nicole Walsh <160256406+nlwlynn@users.noreply.github.com>
2025-01-21 16:08:37 -0500

    Add files via upload

---
+++ AddContact.php
@@ -0,0 +1,40 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$userPhoneNum = $inData["userPhoneNum"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (userPhoneNum, firstName, lastName) VALUES(?,?)");
+		$stmt->bind_param("ss", $userPhoneNum, $firstName, $lastName);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
