1738b5f05ea3185055823a58ab2b7b7fff313d26
rockbison1230 <amanamartin8@gmail.com>
2025-01-27 17:40:22 -0500

    added functionality to login and signup

---
+++ js/code.js
@@ -0,0 +1,157 @@
+const urlBase = 'http://leineckerspages.xyz/LAMPAPI';
+const extension = 'php';
+/*need to add validation functions for signup and apss and username */
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
+function doLogin()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	
+	let login = document.getElementById("loginName").value;
+	let password = document.getElementById("loginPassword").value;
+//	var hash = md5( password );
+	
+	document.getElementById("loginResult").innerHTML = "";
+
+	let tmp = {login:login,password:password};
+//	var tmp = {login:login,password:hash};
+	let jsonPayload = JSON.stringify( tmp );
+	
+	let url = urlBase + '/Login.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				let jsonObject = JSON.parse( xhr.responseText );
+				userId = jsonObject.id;
+		
+				if( userId < 1 )
+				{		
+					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+					return;
+				}
+		
+				firstName = jsonObject.firstName;
+				lastName = jsonObject.lastName;
+
+				saveCookie();
+	
+				window.location.href = "color.html";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+
+}
+
+
+document.getElementById("hideLogin").addEventListener("click", function(event){
+    event.preventDefault();
+    document.getElementById("loginForm").style.display="none";
+    document.getElementById("signUpform").style.display="flex";
+});
+
+
+function doSignup(){
+firstName = document.getElementById("newFname").value;
+lastName = document.getElementById("newLname").value;
+
+let userName=document.getElementById("newUser").value;
+let password = document.getElementById("newPass").value;
+
+var hash=md5(password);
+
+let tmp= {
+    firstName: firstName,
+    lastName: lastName,
+    login:userName,
+    password: hash
+};
+
+let jsonPayload = JSON.stringify(tmp);
+let url = urlBase + '/SignUp.' + extension;
+let xhr = new XMLHttpRequest();
+xhr.open("POST", url, true);
+xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+xhr.onreadystatechange=function(){
+    if(this.readyState==4){
+        if(this.status == 200){
+            let jsonObject = JSON.parse(xhr.responseText);
+            userId=jsonObject.id;
+            document.getElementById("signupResult").innerHTML = "Sign Up Successful";
+            firstName=jsonObject.firstName;
+            lastName=jsonObject.lastName;
+            saveCookie();
+        }else if(this.status==409){
+            document.getElementById("signupResult").innerHTML = "user already exists";
+            return;
+        }else{
+            document.getElementById("signupResult").innerHTML = "error occurred try again";
+        }
+    }
+};
+
+try{
+    xhr.send(jsonPayload);
+}catch(err){
+    document.getElementById("signupResult").innerHTML = err.message;
+}
+
+}
+
+
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+}
