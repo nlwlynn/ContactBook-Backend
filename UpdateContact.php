6ed726c3ac8afefaa7c2d6883fd28f7d09967cd8
jobritto7 <joannabritto1@gmail.com>
2025-01-28 23:16:06 -0500

    update all fields based on contact id

--- UpdateContact.php
+++ UpdateContact.php
@@ -5,6 +5,7 @@
 	$lastName = $inData["lastName"];
 	$phone = $inData["phone"];
     $email = $inData["email"];
+	$id = $inData["id"];
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error) 
@@ -13,8 +14,8 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("Update Contacts set FirstName = ?, LastName = ?, Phone = ?, Email = ?");
-        bind_param("ssssi", $firstName, $lastName, $phone, $email, $inData["UserID"]);
+		$stmt = $conn->prepare("UPDATE Contacts set FirstName = ?, LastName = ?, Phone = ?, Email = ? Where ID = ?");
+        $stmt->bind_param("sssss", $firstName, $lastName, $phone, $email, $id);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
